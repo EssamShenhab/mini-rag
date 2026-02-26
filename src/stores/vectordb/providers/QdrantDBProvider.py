eb0257a8c8bb9ad69ed4ad7bbc850bdaba47b0c0
EssamShenhab <essam119999@gmail.com>
2026-02-26 22:29:32 +0200

    modified all functions to async functions

--- src/stores/vectordb/providers/QdrantDBProvider.py
+++ src/stores/vectordb/providers/QdrantDBProvider.py
@@ -21,26 +21,26 @@ class QdrantDBProvider(VectorDBInterface):
 
         self.logger = logging.getLogger(__name__)
 
-    def connect(self):
+    async def connect(self):
         self.client = QdrantClient(path=self.db_path)
 
-    def disconnect(self):
+    async def disconnect(self):
         self.client = None
 
-    def is_collection_existed(self, collection_name: str) -> bool:
+    async def is_collection_existed(self, collection_name: str) -> bool:
         return self.client.collection_exists(collection_name=collection_name)
 
-    def list_all_collections(self) -> List:
+    async def list_all_collections(self) -> List:
         return self.client.get_collections()
 
-    def get_collection_info(self, collection_name: str) -> dict:
+    async def get_collection_info(self, collection_name: str) -> dict:
         return self.client.get_collection(collection_name=collection_name)
 
-    def delete_collection(self, collection_name: str):
+    async def delete_collection(self, collection_name: str):
         if self.is_collection_existed(collection_name=collection_name):
             return self.client.delete_collection(collection_name=collection_name)
 
-    def create_collection(
+    async def create_collection(
         self, collection_name: str, embedding_size: int, do_reset: bool = False
     ):
         if do_reset:
@@ -59,7 +59,7 @@ class QdrantDBProvider(VectorDBInterface):
 
         return False
 
-    def insert_one(
+    async def insert_one(
         self,
         collection_name: str,
         text: str,
@@ -94,7 +94,7 @@ class QdrantDBProvider(VectorDBInterface):
 
         return True
 
-    def insert_many(
+    async def insert_many(
         self,
         collection_name: str,
         texts: list,
@@ -146,7 +146,7 @@ class QdrantDBProvider(VectorDBInterface):
 
         return True
 
-    def search_by_vector(
+    async def search_by_vector(
         self,
         collection_name: str,
         vector: list,
