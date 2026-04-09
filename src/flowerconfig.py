22d0a1907f61577abd32107c4bf4e045268a1653
EssamShenhab <essam119999@gmail.com>
2026-04-09 13:43:12 +0200

    feat: add Flower dashboard configuration and integrate task indexing module

---
+++ src/flowerconfig.py
@@ -0,0 +1,13 @@
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+
+# Flower configration
+port = 5555
+max_tasks = 10000
+auto_refresh = True
+# db = 'flower.db'   # SQLite database for persistent storage
+
+# Authentication
+basic_auth = [f'admin:{config["CELERY_FLOWER_PASSWOED"]}']
